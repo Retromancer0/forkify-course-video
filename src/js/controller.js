dd1cddcdce85d00efc37db1449f0eb32d1de9311
Retromancer0 <abdullahnmian@gmail.com>
2024-08-15 19:30:21 -0500

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,6 +131,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the applicaiton');
+};
+
 const init = function () {
   BookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -139,5 +143,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   PaginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
